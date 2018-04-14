3412e4469309cdb5c97d919cd6fb8cb1519105dd
Govind Jeevan <govindjeevan7@gmail.com>
2018-04-14 11:47:05 +0530

    Multilevel Files

---
+++ Multilevel ASK/wholecode.m
@@ -0,0 +1,218 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                        CO250-MINI PROJECT
+
+%Performance analysis of state-of-the-art Digital-to-Analog Conversion
+%techniques using Binary Amplitude Shift Keying (BASK) and Multi Amplitude
+%Shift Keying 
+
+%MEMBERS:
+%1. PALAK SINGHAL  16CO129
+%2. GOVIND JEEVAN  16CO221
+%3. PRASANTH SAGAR 16CO225
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                        MAIN FILE
+
+clc;
+close all;
+global Percentage 
+Percentage = [];
+
+global bp
+global ss;
+global f;
+%maximum length of the message
+maxlength=5;
+
+for l=1:2^maxlength   % ALL VALUES FROM 1 BIT TO maxlength BITS
+    x = de2bi(l)       % MESSAGE SIGNAL GENERATED
+    bask(x);           % PERFORMING BINARY AMPLITUDE SHIFT KEYING
+end
+
+ 
+%Plottting length of message vs percentage error detection based on no of bits transmitted correctly
+len=1:length(Percentage);
+figure
+plot(len, Percentage) 
+%calculating the average error detection
+mean(Percentage)
+
+
+
+% BINARY AMPLITUDE SHIFT KEYING
+function output=bask(x)
+    global bp;
+    global ss;
+    global f;
+    global Percentage;
+
+    bp=.000001;                                                    % bit period
+    disp(' Binary information at Trans mitter :');
+    disp(x);
+
+    %XX representation of transmitting binary information as digital signal XXX
+    bit = binary_to_digital(x);
+
+    t1=bp/100:bp/100:100*length(x)*(bp/100);
+    subplot(5,1,1);
+    plot(t1,bit,'lineWidth',2.5);grid on;
+    axis([ 0 bp*length(x) -.5 1.5]);
+    ylabel('amplitude(volt)');
+    xlabel(' time(sec)');
+    title('transmitting information as digital signal');
+
+    m=binary_modulator(10,5,x);
+
+    t3=bp/99:bp/99:bp*length(x);
+    subplot(5,1,2);
+    plot(t3,m);
+    xlabel('time(sec)');
+    ylabel('amplitude(volt)');
+    title('waveform for binary ASK modulation coresponding binary information');
+
+    [m,n] = noise_generator(10, m);
+
+    subplot(5,1,3);
+    plot(t3,n);
+    xlabel('time');
+    ylabel('Noise Amplitude');
+    title('Noise Signal');
+    
+    subplot(5,1,4);
+    plot(t3,m);
+    xlabel('time');
+    ylabel('amplitude');
+    title('Received Signal');
+
+
+    mn=binary_demodulator(m);
+
+    %XXXXX Representation of binary information as digital signal which achived 
+    %after ASK demodulation XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    bit = binary_to_digital(mn);
+
+    t4=bp/100:bp/100:100*length(mn)*(bp/100);
+    subplot(5,1,5);
+    plot(t4,bit,'LineWidth',2.5);grid on;
+    axis([ 0 bp*length(mn) -.5 1.5]);
+    ylabel('amplitude(volt)');
+    xlabel(' time(sec)');
+    title('recived information as digital signal after binary ASK demodulation');
+
+    Percentage=[Percentage, sum(xor(mn,x))/length(x)*100];
+end
+
+
+
+
+%%%% HELPER FUNCTIONS %%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%                          Binary-ASK modulation 
+
+function m = binary_modulator(A1,A2,x)
+% A1 Amplitude of carrier signal for information 1
+% A2 Amplitude of carrier signal for information 0
+% x Message signal
+
+global bp;
+global ss;
+global f;
+br=1/bp;                                                         % bit rate
+f=br*10;                                                 % carrier frequency 
+t2=bp/99:bp/99:bp;                                       % time period to transmit one bit of information
+ss=length(t2);                                           %length of time period
+m=[];                                                    %matrix to store the modulated signal
+
+%the loop runs for the length of the message where each iteration of the
+%loop adds an array of elements which gives the analog signal for time
+%period t2 based on the message signal by imposing it on the carroier
+%signal of frequency f
+for (i=1:1:length(x))
+    if (x(i)==1)
+        y=A1*cos(2*pi*f*t2);  
+    else
+        y=A2*cos(2*pi*f*t2);
+    end
+    m=[m y];
+end
+
+
+end 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+%XXXXXXXXXXXXXXXXXXXXXXX NOISE INTRODUCTION XXXXXXXXXXXXXXXXXXXXXXXXXXX%
+
+function [m,r] = noise_generator(A, x)
+    % A Amplitude of Noise
+    r=A*randn(1,length(x));
+    m=x+r;
+    %To introduce noise in our modulated carrier signal we add the random value
+    %to the signal and assign it to m(the modulated signal)
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%XXXXXXXXXXXXXXXXXXXX Binary ASK demodulation XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+function mn=binary_demodulator(m)
+    global ss;
+    global f;
+    global bp;
+    mn=[];
+    %looping from start of time till the end of the signal and incrementing in units of the time period for 1 bit of information
+    for n=ss:ss:length(m);
+      %dot multiplication of the carrier signal with the 100 successive elemnts of the matrix m which represents the modulated signal
+      %mathematically it will give (A1orA2)cos^2(2*pi*f*t) 
+      t=bp/99:bp/99:bp;
+      y=cos(2*pi*f*t);
+      % carrier siignal
+      mm=y.*m((n-(ss-1)):n);
+      t4=bp/99:bp/99:bp;
+      %trapezoidal integration of the above term with respect to the time period t4
+      z=trapz(t4,mm);
+      %on integration we can ignore the term with sin component as sine can range only from -1 to 1 and further it gets divided by 4*pi*f which gives an extremely small value
+      zz=round((2*z/bp));
+      %logic level (A1+A2)/2=7.5
+      %If the amplitude of the modulated wave is greater than 7.5 it means the priginal message bit was a 1 otherwise it was a 0           
+      if(zz>7.5)                                
+        a=1;
+      else
+        a=0;
+      end
+      mn=[mn a];
+end
+
+end
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%                       Binary to Digital Signal
+
+function bit = binary_to_digital(mn)
+%Conversion of binary message to a binary signal
+%Definign an empty array called bit
+    bit=[]; 
+%For each unit of message we check whether it's a one or zero and on the
+%basis of that we fill a matrix of dimensions 1*100 with that particular
+%value and later adding it to the bit array
+    for n=1:length(mn)
+        if mn(n)==1;
+           se=ones(1,100);
+        else mn(n)==0;
+            se=zeros(1,100);
+        end
+         bit=[bit se];
+
+    end
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
